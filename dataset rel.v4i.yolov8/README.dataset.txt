b3eb04162891840f5af29177918dc8f499ff47c4
RezaGusti00 <rezagusti.skripsi@gmail.com>
2025-03-24 23:07:00 +0700

    Update datashet

---
+++ dataset rel.v4i.yolov8/README.dataset.txt
@@ -0,0 +1,6 @@
+# dataset rel > 2025-03-24 12:23pm
+https://universe.roboflow.com/relll/dataset-rel
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ lane_detection.py
@@ -0,0 +1,25 @@
+from ultralytics import YOLO
+import cv2
+
+# Load model YOLOv8 Instance Segmentation
+model = YOLO("yolov8n-seg.pt")
+
+def detect_rail_lane(image_path):
+    """
+    Mendeteksi jalur rel menggunakan YOLOv8 Instance Segmentation.
+    
+    Args:
+        image_path (str): Path ke file gambar.
+    
+    Returns:
+        None: Hasil deteksi disimpan sebagai file gambar.
+    """
+    # Melakukan deteksi menggunakan model YOLO
+    results = model(image_path, show=True)
+    
+    # Menyimpan hasil deteksi ke file
+    results[0].save("lane_detection_result.jpg")
+
+# Contoh penggunaan
+if __name__ == "__main__":
+    detect_rail_lane("IMG_1144.jpg")
